ba733db16455c2417df36c86381d4ed0089bbeb7
Leonor Oliveira <leonorfmartins@gmail.com>
2019-01-27 13:09:39 +0100

    Step 6.1: Adapt handler to be used in all components

--- src/App.js
+++ src/App.js
@@ -18,12 +18,7 @@ class App extends Component {
         Buttons, Buttons!
       </h2>
       <div>
-        <Button color={'green'} onClick={() => this.setColor('green')}>Green</Button>
-        {/*
-        TODO: Change the onClick prop for your buttons to avoid the duplication of the color value in the color and onClick props.
-        This could easily lead to errors.
-        Hint: You can use the color prop to decide which color to set in the onClick handler
-        */}
+        <Button color={'green'} onClick={this.setColor}>Green</Button>
       </div>
     </div>
     );
--- src/Button.js
+++ src/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
   return (
     <button
       style={{ backgroundColor: color }}
-      onClick={onClick}
+      onClick={() => onClick(color)}
     >
       {children}
     </button>

774ed2625bf02344080f0b8ed2af0351be5b534e
Leonor Oliveira <leonorfmartins@gmail.com>
2019-01-27 12:53:57 +0100

    Step 5: Introduction to local state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import Button from './Button.js';
 
 class App extends Component {
+  state = {
+    selectedColor: 'white',
+    // TODO: change manually the color in the state to a different one!
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App"style={{ backgroundColor: this.state.selectedColor}}>
       <h2>
         Buttons, Buttons!
       </h2>
       <div>
         <Button color={'green'} onClick={() => console.log('I am reacting!')}>Green</Button>
-        {/* TODO: Use children for your component! */}
       </div>
     </div> 
     );

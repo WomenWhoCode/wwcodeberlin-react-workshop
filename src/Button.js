WomenWhoCode/buttonsworkshop

Step 3: Functions as props

src/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Button = (props) => {
-  const { color } = props
+  const { color, onClick } = props
   return (
-    <button style={{ color: color }}></button>
+    <button style={{ color: color }} onClick={onClick}></button>
   )
 }
 

WomenWhoCode/wwcodeberlin-react-workshop

src/Button.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Button = (props) => {
-  const { color } = props
+  const { color, onClick } = props
   return (
-    <button style={{ backgroundColor: color }}></button>
+    <button style={{ backgroundColor: color }} onClick={onClick}></button>
   )
 }
 
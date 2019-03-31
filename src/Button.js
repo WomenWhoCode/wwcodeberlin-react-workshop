WomenWhoCode/buttonsworkshop

Step 2: Props + Component reuse

src/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Button = () => {
+const Button = (props) => {
+  const { color } = props
   return (
-    <button></button>
+    <button style={{ color: color }}></button>
   )
 }
 

WomenWhoCode/wwcodeberlin-react-workshop

src/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Button = () => {
+const Button = (props) => {
+  const { color } = props
   return (
-    <button></button>
+    <button style={{ backgroundColor: color }}></button>
   )
 }
 